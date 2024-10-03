7dd02744e363c1f657ee390d928683f0912f7bd2
Reece Harris <reece.harris98@protonmail.com>
2024-10-03 02:40:26 +0100

    Add initial project files and configurations

---
+++ src/components/nav.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { getCurrentWindow } from "@tauri-apps/api/window";
+
+    onMount(() => {
+        const appWindow = getCurrentWindow();
+
+        document
+            .getElementById("titlebar-minimize")
+            ?.addEventListener("click", () => appWindow.minimize());
+        document
+            .getElementById("titlebar-maximize")
+            ?.addEventListener("click", () => appWindow.toggleMaximize());
+        document
+            .getElementById("titlebar-close")
+            ?.addEventListener("click", () => appWindow.close());
+    });
+</script>
+
+<nav
+    data-tauri-drag-region
+    class="select-none flex gap-2 p-3 justify-start top-0 right-0 left-0 text-white border-b border-zinc-700"
+>
+
+<div class="cursor-pointer bg-red-400 w-3 h-3 rounded-full" id="titlebar-close"></div>
+<div class="cursor-pointer bg-amber-300 w-3 h-3 rounded-full" id="titlebar-maximize"></div>
+<div class="cursor-pointer bg-green-400 w-3 h-3 rounded-full" id="titlebar-minimize"></div>
+
+</nav>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    // https://www.youtube.com/watch?v=m_d7K28CSXg&t=331s
+    
+    import Nav from "$components/nav.svelte";
+    import "../app.css";
+</script>
+
+<main class="bg-zinc-800 w-screen h-screen">
+    <Nav />
+    <slot />
+</main>
